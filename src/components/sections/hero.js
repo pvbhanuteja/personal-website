7c0324efa26173cedca10881f34bd3ec9fbd77c3
Bhanu <pvbhanuteja@gmail.com>
2022-09-14 13:35:02 -0500

    intern date fix

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -65,7 +65,7 @@ const Hero = () => {
   const four = (
     <>
       <p>
-        I’m an ML researcher and engineer. Currently, a graduate student at
+        I’m AI/ML researcher and engineer. Currently, a graduate student at
         {' '}
         <a href="https://tamu.edu/" target="_blank" rel="noreferrer">
           Texas A{' & '}M University
